5f210ec6ea45f99bdffb2470430aea1f03a2cabb
bogdan sivochkin <syi3@ya.ru>
2021-03-12 19:55:23 +0300

    initial

---
+++ config/config.go
@@ -0,0 +1,81 @@
+package config
+
+import (
+	"flag"
+	//"context"
+	"fmt"
+	"github.com/spf13/viper"
+	"time"
+)
+
+// Config is global object that holds all application level variables.
+var Config AppConfig
+
+type AppConfig struct {
+	// Example Variable
+	PORT                    int
+	LISTEN_NETWORK          string
+	POOL_ADDRESS            string
+	ORACLE_CONTRACT_ADDRESS string
+	TOKENPOOL_ADDRESS       string
+	BRIDGE_ADDRESS          string
+	CHAIN_1_URL             string
+	CHAIN_2_URL             string
+	INFURA_URL              string
+	TickerInterval          time.Duration
+}
+
+func ReadConfig() error {
+	viper.SetConfigName("my") // name of config file (without extension)
+	viper.SetEnvPrefix("cross-chain")
+	viper.SetConfigType("yaml")         // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("../../config") // path to look for the config file in
+	viper.AddConfigPath("$HOME")        // call multiple times to add many search paths
+	viper.AddConfigPath(".")            // optionally look for config in the working directory
+	err := viper.ReadInConfig()         // Find and read the config file
+	if err != nil {                     // Handle errors reading the config file
+		//panic(fmt.Errorf("Fatal error config file: %s \n", err))
+		return err
+	}
+	return nil
+}
+
+// LoadConfig loads config from files
+func LoadConfig(configPaths ...string) error {
+	v := viper.New()
+	v.SetConfigName("my")
+	v.SetConfigType("yaml")
+	v.SetEnvPrefix("cross-chain")
+	v.AutomaticEnv()
+	v.AddConfigPath("../")
+	v.AddConfigPath("../config")
+	v.AddConfigPath("../../config")
+	for _, path := range configPaths {
+		v.AddConfigPath(path)
+		fmt.Print("path", path, "\n")
+	}
+	err := v.ReadInConfig()
+	if err != nil {
+		return err
+	}
+
+	return v.Unmarshal(&Config)
+}
+
+func LoadConfigAndArgs() (cfg *AppConfig) {
+	cfg = NewConfig()
+	LoadConfig(".")
+	return cfg
+}
+
+func NewConfig() *AppConfig {
+	c := AppConfig{}
+	flag.IntVar(&c.PORT, "p", 8080, "Port number to serve")
+	flag.DurationVar(
+		&c.TickerInterval,
+		"t",
+		time.Minute,
+		"Ticker interval for the adaptor to refresh supported trading pairs, suggested units: s, m, h")
+	flag.Parse()
+	return &c
+}
---
+++ transmitter-adapter/2.go
@@ -0,0 +1,54 @@
+package transmitter_adapter
+
+import (
+	"context"
+	"fmt"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/linkpoolio/bridges"
+	"github.com/sivo4kin/digiu-cross-chain/config"
+	"log"
+	"net/http"
+)
+
+type Adapter2 struct {
+	CLient *ethclient.Client
+	Config config.AppConfig
+
+	//Session         *bindings.Session
+	//Filterer *bindings.Filterer
+}
+
+func (ap *Adapter2) Run(helper *bridges.Helper) (interface{}, error) {
+	return GetBlocks2, nil
+}
+
+func NewAdapter2(cfg config.AppConfig) (a *Adapter2, err error) {
+	a = &Adapter2{}
+	a.Config = cfg
+	a.CLient, err = ethclient.Dial(config.Config.INFURA_URL)
+	return
+}
+
+func (ap *Adapter2) Opts() *bridges.Opts {
+	return &bridges.Opts{
+		Name:   "post",
+		Lambda: true,
+		Path:   "/control",
+	}
+}
+
+func GetBlocks2() error {
+	header, err := Adapter2{}.CLient.HeaderByNumber(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	fmt.Printf("current block %s", header.Number.String())
+	return nil
+}
+
+func Handler2(g http.ResponseWriter, r *http.Request) error {
+	log.Print("handler 3")
+	return nil
+	//bridges.NewServer(da.Handle(w, r))
+}
