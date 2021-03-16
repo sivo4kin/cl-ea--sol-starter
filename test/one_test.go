9d7bfcb0e7967f08d68857d15d39516020c7cc90
bogdan sivochkin <syi3@ya.ru>
2021-03-16 12:04:51 +0300

    first test

---
+++ test/one_test.go
@@ -0,0 +1,36 @@
+package test
+
+import (
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/sirupsen/logrus"
+	"github.com/sivo4kin/digiu-cross-chain/build/bindings"
+
+	//"github.com/ethereum/go-ethereum/eth/protocols/eth"
+	"github.com/stretchr/testify/require"
+	"math/big"
+	"testing"
+)
+
+func Test(t *testing.T){
+	ethereumKey, _ := crypto.GenerateKey()
+	auth, err := bind.NewKeyedTransactorWithChainID(ethereumKey, big.NewInt(1337))
+	require.NoError(t, err)
+	var genesisData = core.GenesisAlloc{auth.From: {Balance: big.NewInt(1000000000)}}
+	gasLimit := uint64(100000000)
+	backend := backends.NewSimulatedBackend(genesisData, gasLimit)
+	require.NotNil(t, backend)
+	addr, _, dToken, err := bindings.DeployDigiUToken(auth, backend)
+	require.NoError(t, err)
+	require.NotNil(t, addr)
+	logrus.Printf("Token deployed 0x%x", addr)
+	backend.Commit()
+	bal, err := dToken.BalanceOf(&bind.CallOpts{}, common.HexToAddress("0"))
+	//_, err = verifier.RandomValueFromVRFProof(nil, proof[:])
+	require.NoError(t, err)
+	logrus.Printf("balance %d", bal)
+
+}
